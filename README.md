a4daf3cd2f9c16fc12e138cf8eb8bc267d4bce8b
Gopinath Munsifreddy <gmunisifreddy@expedia.com>
2014-08-20 13:15:57 -0700

    Added random forest based prediction script and updated readme.md file

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-kaggle-bike-sharing-demand-r
-============================
+Solving Kaggle Bike Sharing Demand problem using R language
+==========================================================
 
-Solving Kaggle Bike Sharing Demand prediction problem in R language using random forests
+The R script available in src folder helps you to solve the bike sharing demand problem using random forests with RMSLE score of 0.70117. That should place in somewhere in 300th to 400th position on leader board.
+
+You are free to copy the code and improve on.
---
+++ src/random-forest.r
@@ -0,0 +1,37 @@
+#
+# Project: Predict Bike Sharing Demand using R
+# Author : Gopinath Munisifreddy
+# Date   : Aug-18-2014
+#
+
+#Set your working folder - CHANGE THIS PATH APPROPRIATELY
+setwd("D:\\data-science\\bikesharing\\")
+
+#Loading training file as well as test file - CHANGE THIS PATH APPROPRIATELY
+train <- read.csv("D:\\data-science\\bikesharing\\train.csv")
+test <- read.csv("D:\\data-science\\bikesharing\\test.csv")
+
+#install required libraries
+install.packages("lubridate") 
+install.packages('randomForest')
+library(lubridate)
+library(randomForest)
+
+#Compute hour of the day
+train$hour <-hour(train$datetime)
+test$hour <- hour(test$datetime)
+
+#Compute day of the week
+train$dow <- wday(train$datetime)
+test$dow <- wday(test$datetime)
+
+test$count<-0
+#Create a random forest
+fit <- randomForest(as.factor(count) ~ season + holiday + weather + dow+ hour + temp + atemp+humidity+windspeed , data=train, ntree = 700, importance=TRUE)
+#Uncomment the following line if you want to see how your model plot looks like
+#varImpPlot(fit)
+
+#Predict values and save output
+Prediction <- predict(fit, test)
+submit <- data.frame(datetime = test$datetime, count = Prediction)
+write.csv(submit, file = "random-forest.csv", row.names = FALSE)

2997f8da642ad359c092451f2b1126cb992b1a32
Bill Cary <bill.cary@gmail.com>
2014-09-30 17:00:48 -0400

    Set up H2O model.

--- src/install.R
+++ src/install.R
@@ -1,11 +1,13 @@
 
 
-install.packages('lubridate', dependencies=TRUE) 
-install.packages('randomForest', dependencies=TRUE)
-install.packages('caret', dependencies=TRUE)
-install.packages('Metrics', dependencies=TRUE)
-install.packages('doParallel', dependencies=TRUE)
-install.packages('devtools', dependencies = TRUE)
+install.packages('lubridate', dependencies=TRUE, quiet = TRUE) 
+install.packages('randomForest', dependencies=TRUE, quiet = TRUE)
+install.packages('caret', dependencies=TRUE, quiet = TRUE)
+install.packages('Metrics', dependencies=TRUE, quiet = TRUE)
+install.packages('doParallel', dependencies=TRUE, quiet = TRUE)
+install.packages('devtools', dependencies = TRUE, quiet = TRUE)
 
 library(devtools)
 install_url(url = 'http://cran.r-project.org/src/contrib/Archive/weathermetrics/weathermetrics_1.0.tar.gz')
+
+install.packages("h2o", repos=(c("http://h2o-release.s3.amazonaws.com/h2o/rel-lambert/5/R", getOption("repos"))), quiet = TRUE)

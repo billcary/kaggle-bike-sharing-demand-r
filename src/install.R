1418b7f9b15846aa66c682f72462c9e4d3689f98
Bill Cary <bill.cary@gmail.com>
2014-12-25 20:40:08 -0500

    Add gridExtra to list of packages to be installed when running on cloud-based systems.

--- src/install.R
+++ src/install.R
@@ -8,6 +8,7 @@ install.packages('doParallel', dependencies=TRUE, quiet = TRUE)
 install.packages('devtools', dependencies = TRUE, quiet = TRUE)
 install.packages('gbm', dependencies = TRUE, quiet = TRUE)
 install.packages('Cubist', dependencies = TRUE, quiet = TRUE)
+install.packages('gridExtra', dependencies = TRUE, quiet = TRUE)
 
 library(devtools)
 install_url(url = 'http://cran.r-project.org/src/contrib/Archive/weathermetrics/weathermetrics_1.0.tar.gz')

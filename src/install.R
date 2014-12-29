0437d562a9ae34fdb1ca84eff42ce89e90e9b26b
Bill Cary <bill.cary@gmail.com>
2014-12-28 23:48:44 -0500

    Add deepnet package.

--- src/install.R
+++ src/install.R
@@ -10,6 +10,7 @@ install.packages('gbm', dependencies = TRUE, quiet = TRUE)
 install.packages('Cubist', dependencies = TRUE, quiet = TRUE)
 install.packages('gridExtra', dependencies = TRUE, quiet = TRUE)
 install.packages('DataCombine', dependencies = TRUE, quiet = TRUE)
+install.packages('deepnet', dependencies = TRUE, quiet = TRUE)
 
 library(devtools)
 install_url(url = 'http://cran.r-project.org/src/contrib/Archive/weathermetrics/weathermetrics_1.0.tar.gz')
